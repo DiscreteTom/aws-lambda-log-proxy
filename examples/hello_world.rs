0e7cc31196d33b94602dd4e12cdac573609786c3
DiscreteTom <discrete_tom@outlook.com>
2024-04-04 17:16:39 +0800

    chore: add examples/hello_world

---
+++ examples/hello_world.rs
@@ -0,0 +1,26 @@
+use aws_lambda_log_proxy::{LogProxy, Sink};
+
+#[tokio::main]
+async fn main() {
+  // Create a sink to write log lines to stdout.
+  let sink = Sink::stdout();
+
+  LogProxy::default()
+    .stdout(|processor| {
+      processor
+        // only keep lines that contains "hello"
+        .filter(|line| line.contains("hello"))
+        // clone the sink instead of creating a new one
+        // to prevent interleaved output
+        .sink(sink.clone())
+    })
+    .stderr(|processor| {
+      processor
+        // prepend "ERROR: " to each line
+        .transformer(|line| Some(format!("ERROR: {line}")))
+        // also write to the `stdout` sink
+        .sink(sink)
+    })
+    .start()
+    .await;
+}
