a957d08d28abfd40d46920421b83c12d887aaeec
DiscreteTom <discrete_tom@outlook.com>
2024-04-09 06:27:28 +0000

    bench: optimize validate.sh

--- benchmark/validate.sh
+++ benchmark/validate.sh
@@ -3,16 +3,20 @@
 params=(10 100 1000 10000 100000)
 for param in "${params[@]}"
 do
+  echo "validating: LambdaLogProxy${param}EnabledFunction"
   for ((i=1; i<=10; i++))
   do
     # ensure 'done' is printed
-    if sam remote invoke "LambdaLogProxy${param}EnabledFunction" --stack-name LambdaLogProxyBenchmark 2>&1 | tail -n 4 | grep -q "done"; then
+    output=`sam remote invoke "LambdaLogProxy${param}EnabledFunction" --stack-name LambdaLogProxyBenchmark 2>&1 | tail -n 4`
+    if echo "$output" | grep -q "done"; then
       echo -n "."
     else
-      echo "validation failed in LambdaLogProxy${param}EnabledFunction"
+      echo "validation failed, output of last 4 lines:"
+      echo "$output"
       exit 1
     fi
   done
+  echo "" # append newline
 done
 
 
