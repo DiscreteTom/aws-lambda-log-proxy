bf26cb5d60f49b995341e486915be26544c72ee9
DiscreteTom <discrete_tom@outlook.com>
2024-04-16 18:17:20 +0800

    chore: optimize code for examples/suppress

--- examples/suppress.rs
+++ examples/suppress.rs
@@ -4,28 +4,33 @@
 //! Synchronous invoke of the lambda function (e.g. API Gateway) will get the response
 //! immediately but the lambda will run for 2 more seconds to process the log lines.
 
-use aws_lambda_log_proxy::{LogProxy, Processor};
+use aws_lambda_log_proxy::{LogProxy, Processor, Sink, SinkHandle};
 use std::time::Duration;
 use tokio::time::sleep;
 
-pub struct MyProcessor;
+// SinkHandle is clone-able so our processor is also clone-able.
+#[derive(Clone)]
+pub struct MyProcessor(SinkHandle);
 
 impl Processor for MyProcessor {
-  async fn process(&mut self, line: String, _timestamp: i64) -> bool {
+  async fn process(&mut self, line: String, timestamp: i64) -> bool {
     sleep(Duration::from_secs(1)).await;
-    println!("{}", line);
+    self.0.write_line(line, timestamp).await;
     true
   }
   async fn flush(&mut self) {
     sleep(Duration::from_secs(1)).await;
+    self.0.flush().await;
   }
 }
 
 #[tokio::main]
 async fn main() {
+  let processor = MyProcessor(Sink::stdout().spawn());
+
   LogProxy {
-    stdout: Some(MyProcessor),
-    stderr: Some(MyProcessor),
+    stdout: Some(processor.clone()),
+    stderr: Some(processor),
     ..Default::default()
   }
   .start()
