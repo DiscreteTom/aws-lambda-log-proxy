ead08eb429ac92095775d071d9ba1f5649eddaa0
DiscreteTom <discrete_tom@outlook.com>
2024-04-16 18:17:30 +0800

    chore: add examples/conditional_sink

---
+++ examples/conditional_sink.rs
@@ -0,0 +1,42 @@
+//! The SimpleProcessor can only have one sink.
+//! But with custom processor you can have conditional sink.
+
+use aws_lambda_log_proxy::{LogProxy, Processor, Sink, SinkHandle};
+
+// SinkHandle is clone-able so our processor is also clone-able.
+#[derive(Clone)]
+pub struct MyProcessor {
+  pub stdout_sink: SinkHandle,
+  pub stderr_sink: SinkHandle,
+}
+
+impl Processor for MyProcessor {
+  async fn process(&mut self, line: String, timestamp: i64) -> bool {
+    if line.starts_with("out") {
+      self.stdout_sink.write_line(line, timestamp).await;
+    } else {
+      self.stderr_sink.write_line(line, timestamp).await;
+    }
+    true
+  }
+  async fn flush(&mut self) {
+    self.stdout_sink.flush().await;
+    self.stderr_sink.flush().await;
+  }
+}
+
+#[tokio::main]
+async fn main() {
+  let processor = MyProcessor {
+    stdout_sink: Sink::stdout().spawn(),
+    stderr_sink: Sink::stderr().spawn(),
+  };
+
+  LogProxy {
+    stdout: Some(processor.clone()),
+    stderr: Some(processor),
+    ..Default::default()
+  }
+  .start()
+  .await;
+}
