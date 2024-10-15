ffe09a919d4dc5836d2dc220880e89b8e1ce38f8
DiscreteTom <discrete_tom@outlook.com>
2024-10-15 02:50:21 +0000

    examples: update suppress

--- examples/suppress.rs
+++ examples/suppress.rs
@@ -1,3 +1,6 @@
+//! This example demonstrates how to the log proxy can be used
+//! to suppress invocation/next until all the logs are processed.
+
 use aws_lambda_log_proxy::{LogProxy, Processor, Sink, SinkHandle, Timestamp};
 use std::time::Duration;
 use tokio::time::sleep;
@@ -8,18 +11,19 @@ pub struct MyProcessor(SinkHandle);
 
 impl Processor for MyProcessor {
   async fn process(&mut self, line: String, timestamp: Timestamp) {
-    sleep(Duration::from_secs(1)).await;
+    sleep(Duration::from_secs(1)).await; // simulate processing time
     self.0.write_line(line, timestamp).await;
   }
   async fn truncate(&mut self) {
-    sleep(Duration::from_secs(1)).await;
+    sleep(Duration::from_secs(1)).await; // simulate processing time
     self.0.flush().await;
   }
 }
 
 #[tokio::main]
 async fn main() {
-  let processor = MyProcessor(Sink::stdout().spawn());
-
-  LogProxy::new().processor(processor).start().await;
+  LogProxy::new()
+    .processor(MyProcessor(Sink::stdout().spawn()))
+    .start()
+    .await;
 }
